44ad9577a469400da530df6c9940dad6a8714954
chaoukat99 <chawkatomar@gmail.com>
2025-11-17 22:22:25 +0100

    17/11/2025 commit 2

--- dom.js
+++ dom.js
@@ -79,7 +79,7 @@ let myH1=document.querySelector("#titre");
 // console.log(h1text);
 
 // Update content
-myH1.innerHTML="<b style='color:red'>Hello gomycode</b>";
+myH1.innerHTML="Hello gomycode";
 
 
 
@@ -101,82 +101,165 @@ myH1.innerHTML="<b style='color:red'>Hello gomycode</b>";
 // mypara.innerHTML=userPrompt;
 
 
-function Addition(n1,n2){
-    return n1+n2
+// function Addition(n1,n2){
+//     return n1+n2
+// }
+// function Sous(n1,n2){
+//     return n1-n2
+// }
+
+
+// function Multi(n1,n2){
+//     return n1*n2;
+// }
+
+
+// function Division(n1,n2){
+//     if(n2!=0){
+//         return n1/n2
+//     }
+//      alert("Impossible")
+// }
+
+// function Calculator(num1,opearator,num2){
+//      let numOne=document.querySelector(".num1");
+//      let numTwo=document.querySelector(".num2");
+//      let op=document.querySelector(".opeartor");
+//      let result=document.querySelector(".result")
+// let maindiv=document.querySelector(".calculatrice");
+//     switch(opearator){
+//         case "+":{
+//         numOne.innerHTML=num1;
+//         op.innerHTML=opearator;
+//         numTwo.innerHTML=num2;
+//         result.innerHTML=Addition(num1,num2)
+
+
+//         }break;
+//         case "-":{
+//         numOne.innerHTML=num1;
+//         op.innerHTML=opearator;
+//         numTwo.innerHTML=num2;
+//         result.innerHTML=Sous(num1,num2)
+
+
+//         }break;
+//         case "x":{
+//         numOne.innerHTML=num1;
+//         op.innerHTML=opearator;
+//         numTwo.innerHTML=num2;
+//         result.innerHTML=Multi(num1,num2)
+
+
+//         }break;
+//         case "/":{
+//         numOne.innerHTML=num1;
+//         op.innerHTML=opearator;
+//         numTwo.innerHTML=num2;
+//         result.innerHTML=Division(num1,num2)
+
+
+//         }break;
+
+//         default:{
+// maindiv.innerHTML="Invalid Operator"
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// let p1=prompt("num1 : ")
+// let p2=prompt("operateur : ")
+// let p3=prompt("num2 : ")
+// Calculator(+p1,p2,+p3);
+
+
+
+
+
+// Read attributes
+
+
+let passwordInput=document.querySelector("#pass");
+// element.attributename
+console.log(passwordInput.placeholder)
+
+console.log(passwordInput.getAttribute("name"))
+
+
+// Update attributes
+
+// let mylink=document.querySelector("a");
+
+// console.log(mylink.href)
+
+// mylink.setAttribute("href","https://wa.me")
+
+
+// // Update Href 
+
+// mylink.href="https://gomycode.com/ma/fr/"
+
+// let myp=document.querySelector("p");
+
+// console.log(myp.className)
+
+
+
+// Styling 
+
+
+// 2 method 
+
+
+let h1=document.querySelector("#titre");
+
+
+
+// Style inline 
+
+// element.style.propertcss=value;
+
+// h1.style.color="red";
+
+
+let obj={
+    id:1,
+    name:"hello"
 }
-function Sous(n1,n2){
-    return n1-n2
-}
+console.log(obj.name)
 
 
-function Multi(n1,n2){
-    return n1*n2;
-}
+// h1.style.backgroundColor="black";
+// h1.style.padding="20px";
+// h1.style.borderRadius="2"
+
+// 2éme cssText
 
 
-function Division(n1,n2){
-    if(n2!=0){
-        return n1/n2
-    }
-     alert("Impossible")
-}
-
-function Calculator(num1,opearator,num2){
-     let numOne=document.querySelector(".num1");
-     let numTwo=document.querySelector(".num2");
-     let op=document.querySelector(".opeartor");
-     let result=document.querySelector(".result")
-let maindiv=document.querySelector(".calculatrice");
-    switch(opearator){
-        case "+":{
-        numOne.innerHTML=num1;
-        op.innerHTML=opearator;
-        numTwo.innerHTML=num2;
-        result.innerHTML=Addition(num1,num2)
-
-
-        }break;
-        case "-":{
-        numOne.innerHTML=num1;
-        op.innerHTML=opearator;
-        numTwo.innerHTML=num2;
-        result.innerHTML=Sous(num1,num2)
-
-
-        }break;
-        case "x":{
-        numOne.innerHTML=num1;
-        op.innerHTML=opearator;
-        numTwo.innerHTML=num2;
-        result.innerHTML=Multi(num1,num2)
-
-
-        }break;
-        case "/":{
-        numOne.innerHTML=num1;
-        op.innerHTML=opearator;
-        numTwo.innerHTML=num2;
-        result.innerHTML=Division(num1,num2)
-
-
-        }break;
-
-        default:{
-maindiv.innerHTML="Invalid Operator"
-        }
-    }
-}
+// h1.style.cssText="color:red;background-color:black;padding:10px;"
+// console.log(h1);
 
 
 
+// classList
+
+// contains
+
+// console.log(h1.classList.contains("cls"))
+
+// add
 
 
+document.querySelector("button").addEventListener("click",()=>{
+h1.classList.toggle("active")
+})
 
-
-
-
-
-let p1=prompt("num1 : ")
-let p2=prompt("operateur : ")
-let p3=prompt("num2 : ")
-Calculator(+p1,p2,+p3);
