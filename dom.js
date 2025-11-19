fb9d94679c12c03f804cc5942ddb94df53d653bf
chaoukat99 <chawkatomar@gmail.com>
2025-11-19 12:32:41 +0100

    19/11/2025

--- dom.js
+++ dom.js
@@ -1,63 +1,63 @@
 
-// DOM(document object model)
+// // DOM(document object model)
 
-// Selection 
-// Control tag content 
-// Control attributes
-// manages Styles
-// ClassList
-// Dom traversing
-// Create Elements 
-// Events 
+// // Selection 
+// // Control tag content 
+// // Control attributes
+// // manages Styles
+// // ClassList
+// // Dom traversing
+// // Create Elements 
+// // Events 
 
 
 
 
-// Selection 
+// // Selection 
 
 
-// Selection By Id 
+// // Selection By Id 
 
-// let title=document.getElementById("titre");
+// // let title=document.getElementById("titre");
 
-// console.log(title);
+// // console.log(title);
 
 
-// Selection By className 
+// // Selection By className 
 
 
-// let classeCls=document.getElementsByClassName("cls");
+// // let classeCls=document.getElementsByClassName("cls");
 
-// console.log(classeCls[0]);
+// // console.log(classeCls[0]);
 
 
-// selection By tagname 
+// // selection By tagname 
 
 
-// let paras=document.getElementsByTagName("p");
+// // let paras=document.getElementsByTagName("p");
 
 
-// console.log(paras)
+// // console.log(paras)
 
 
 
-// selectionCss 
+// // selectionCss 
 
-// QuerySelector
+// // QuerySelector
 
-// let mypara=document.querySelector(".cls")
-// console.log(mypara);
+// // let mypara=document.querySelector(".cls")
+// // console.log(mypara);
 
-// QuerySelectorAll
+// // QuerySelectorAll
 
-// let allCls=document.querySelectorAll(".cls");
+// // let allCls=document.querySelectorAll(".cls");
 
-// console.log(allCls)
+// // console.log(allCls)
 
 
 
 
-// let myInput=document.querySelector("input[type='password']");
+// // let myInput=document.querySelector("input[type='password']");
 
 
 
@@ -69,21 +69,23 @@
 
 
 
-let myH1=document.querySelector("#titre");
+// let myH1=document.querySelector("#titre");
 
 
-// read content
-// innerHTML  && innerText
-// const h1text=myH1.innerHTML;
+// // read content
+// // innerHTML  && innerText
+// // const h1text=myH1.innerHTML;
 
-// console.log(h1text);
+// // console.log(h1text);
 
-// Update content
-myH1.innerHTML="Hello gomycode";
+// // Update content
+// myH1.innerHTML="Hello gomycode";
 
 
 
 
+// // let clsElms=document.querySelectorAll(".cls");
+// // clsElms[clsElms.length-1].innerHTML="<b>SDHJSH@HKSHD&é123</b>";
 
 
 
@@ -92,174 +94,284 @@ myH1.innerHTML="Hello gomycode";
 
 
 
-// js 
 
-// let userPrompt=prompt("Insert a text ")
-// let mypara=document.querySelector("#mypara");
 
+// // js 
 
-// mypara.innerHTML=userPrompt;
+// // let userPrompt=prompt("Insert a text ")
+// // let mypara=document.querySelector("#mypara");
 
 
-// function Addition(n1,n2){
-//     return n1+n2
+// // mypara.innerHTML=userPrompt;
+
+
+// // function Addition(n1,n2){
+// //     return n1+n2
+// // }
+// // function Sous(n1,n2){
+// //     return n1-n2
+// // }
+
+
+// // function Multi(n1,n2){
+// //     return n1*n2;
+// // }
+
+
+// // function Division(n1,n2){
+// //     if(n2!=0){
+// //         return n1/n2
+// //     }
+// //      alert("Impossible")
+// // }
+
+// // function Calculator(num1,opearator,num2){
+// //      let numOne=document.querySelector(".num1");
+// //      let numTwo=document.querySelector(".num2");
+// //      let op=document.querySelector(".opeartor");
+// //      let result=document.querySelector(".result")
+// // let maindiv=document.querySelector(".calculatrice");
+// //     switch(opearator){
+// //         case "+":{
+// //         numOne.innerHTML=num1;
+// //         op.innerHTML=opearator;
+// //         numTwo.innerHTML=num2;
+// //         result.innerHTML=Addition(num1,num2)
+
+
+// //         }break;
+// //         case "-":{
+// //         numOne.innerHTML=num1;
+// //         op.innerHTML=opearator;
+// //         numTwo.innerHTML=num2;
+// //         result.innerHTML=Sous(num1,num2)
+
+
+// //         }break;
+// //         case "x":{
+// //         numOne.innerHTML=num1;
+// //         op.innerHTML=opearator;
+// //         numTwo.innerHTML=num2;
+// //         result.innerHTML=Multi(num1,num2)
+
+
+// //         }break;
+// //         case "/":{
+// //         numOne.innerHTML=num1;
+// //         op.innerHTML=opearator;
+// //         numTwo.innerHTML=num2;
+// //         result.innerHTML=Division(num1,num2)
+
+
+// //         }break;
+
+// //         default:{
+// // maindiv.innerHTML="Invalid Operator"
+// //         }
+// //     }
+// // }
+
+
+
+
+
+
+
+
+
+
+// // let p1=prompt("num1 : ")
+// // let p2=prompt("operateur : ")
+// // let p3=prompt("num2 : ")
+// // Calculator(+p1,p2,+p3);
+
+
+
+
+
+// // Read attributes
+
+
+// let passwordInput=document.querySelector("#pass");
+// // element.attributename
+// console.log(passwordInput.placeholder)
+
+// console.log(passwordInput.getAttribute("name"))
+
+
+// // Update attributes
+
+// // let mylink=document.querySelector("a");
+
+// // console.log(mylink.href)
+
+// // mylink.setAttribute("href","https://wa.me")
+
+
+// // // Update Href 
+
+// // mylink.href="https://gomycode.com/ma/fr/"
+
+// // let myp=document.querySelector("p");
+
+// // console.log(myp.className)
+
+
+
+// // Styling 
+
+
+// // 2 method 
+
+
+// let h1=document.querySelector("#titre");
+
+
+
+// // Style inline 
+
+// // element.style.propertcss=value;
+
+// // h1.style.color="red";
+
+
+// let obj={
+//     id:1,
+//     name:"hello"
 // }
-// function Sous(n1,n2){
-//     return n1-n2
-// }
+// console.log(obj.name)
 
 
-// function Multi(n1,n2){
-//     return n1*n2;
-// }
+// // h1.style.backgroundColor="black";
+// // h1.style.padding="20px";
+// // h1.style.borderRadius="2"
 
+// // 2éme cssText
 
-// function Division(n1,n2){
-//     if(n2!=0){
-//         return n1/n2
-//     }
-//      alert("Impossible")
-// }
 
-// function Calculator(num1,opearator,num2){
-//      let numOne=document.querySelector(".num1");
-//      let numTwo=document.querySelector(".num2");
-//      let op=document.querySelector(".opeartor");
-//      let result=document.querySelector(".result")
-// let maindiv=document.querySelector(".calculatrice");
-//     switch(opearator){
-//         case "+":{
-//         numOne.innerHTML=num1;
-//         op.innerHTML=opearator;
-//         numTwo.innerHTML=num2;
-//         result.innerHTML=Addition(num1,num2)
+// // h1.style.cssText="color:red;background-color:black;padding:10px;"
+// // console.log(h1);
 
 
-//         }break;
-//         case "-":{
-//         numOne.innerHTML=num1;
-//         op.innerHTML=opearator;
-//         numTwo.innerHTML=num2;
-//         result.innerHTML=Sous(num1,num2)
 
+// // classList
 
-//         }break;
-//         case "x":{
-//         numOne.innerHTML=num1;
-//         op.innerHTML=opearator;
-//         numTwo.innerHTML=num2;
-//         result.innerHTML=Multi(num1,num2)
+// // contains
 
+// // console.log(h1.classList.contains("cls"))
 
-//         }break;
-//         case "/":{
-//         numOne.innerHTML=num1;
-//         op.innerHTML=opearator;
-//         numTwo.innerHTML=num2;
-//         result.innerHTML=Division(num1,num2)
+// // add
 
 
-//         }break;
+// document.querySelector("button").addEventListener("click",()=>{
+// h1.classList.toggle("active")
+// })
 
-//         default:{
-// maindiv.innerHTML="Invalid Operator"
-//         }
-//     }
-// }
 
 
 
+// // 
 
 
 
 
+// const imagSrcs=["https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLat8bZvhXD3ChSXyzGsFVh6qgplm1KhYPKA&s"]
 
 
 
-// let p1=prompt("num1 : ")
-// let p2=prompt("operateur : ")
-// let p3=prompt("num2 : ")
-// Calculator(+p1,p2,+p3);
+// // let myImage=document.querySelector("img");
 
+// let imgButton=document.querySelector("#imgid");
 
+// // let i=0
+// // imgButton.onclick=()=>{
+// //     let rand=Math.floor(Math.random()*3) 
+// //     myImage.src=imagSrcs[i]
+// //     i++;
+// // }
 
 
+// // let inp=document.querySelector("#passwordinp");
 
-// Read attributes
+// // imgButton.onclick=()=>{
+// //   if(inp.type==="text"){
+// //     inp.type="password"
+// //   }else{
+// //     inp.type="text"
+// //   }
+// // }
 
 
-let passwordInput=document.querySelector("#pass");
-// element.attributename
-console.log(passwordInput.placeholder)
+// // let checkbox=document.querySelector("input[type='checkbox']");
 
-console.log(passwordInput.getAttribute("name"))
+// // console.log(checkbox.checked)
 
+// let notif=100;
+// notif=notif-20
+// document.title=`(${notif}) Whatsapp`;
+// // document.body.style.backgroundColor="black"
 
-// Update attributes
+// // classList
 
-// let mylink=document.querySelector("a");
 
-// console.log(mylink.href)
+// // elm.classList.add("classname")
+// // elm.classList.remove("classname")
+// // elm.classList.toggle("classname")
 
-// mylink.setAttribute("href","https://wa.me")
 
+// // let switchMode=document.querySelector("#mode")
 
-// // Update Href 
+// // switchMode.onclick=()=>{
+// //   document.body.classList.toggle("darkmode")
+// // }
 
-// mylink.href="https://gomycode.com/ma/fr/"
 
-// let myp=document.querySelector("p");
 
-// console.log(myp.className)
+// // forms 
+// // images
+// // links
 
 
+let para=document.querySelector("#myP");
 
-// Styling 
 
+// parentElement = select parent of an element
+// children=list children elements 
+// previousElementSibling = khouk lkbir
+// previousElementSibling = khouk  Sghir
+// console.log(para.parentElement.children);
+// console.log(para.nextElementSibling);
 
-// 2 method 
 
+let inp=document.querySelector("input[type='text'");
 
-let h1=document.querySelector("#titre");
+// console.log(inp.pare)
 
+// childNodes // texte et balises
 
+// parentNode // parent 
+// nextSibling   // khouk sghir b texte
+// previousSibling  // khouk lkbir b texte
 
-// Style inline 
+// let mark = document.querySelector("mark");
+// console.log(mark.previousSibling)
+// console.log(mark.nextSibling)
 
-// element.style.propertcss=value;
 
-// h1.style.color="red";
+// append vs Remove
 
 
-let obj={
-    id:1,
-    name:"hello"
-}
-console.log(obj.name)
 
+let allLinks=document.links;
 
-// h1.style.backgroundColor="black";
-// h1.style.padding="20px";
-// h1.style.borderRadius="2"
+let images=document.images;
 
-// 2éme cssText
+let forms = document.forms;
 
 
-// h1.style.cssText="color:red;background-color:black;padding:10px;"
-// console.log(h1);
+// let myForm=forms[0]
 
 
+// console.log(myForm.age)
 
-// classList
-
-// contains
-
-// console.log(h1.classList.contains("cls"))
-
-// add
-
-
-document.querySelector("button").addEventListener("click",()=>{
-h1.classList.toggle("active")
-})
-
+// 
